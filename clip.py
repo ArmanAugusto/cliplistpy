45100d7dfe1d20d73ab848eca7ce997b3dec3e81
Arman Augusto <arman.augusto@gmail.com>
2024-12-20 07:05:15 -0800

    updated title_header

--- clip.py
+++ clip.py
@@ -4,11 +4,13 @@ from rich.console import Console
 
 def title_header():
     console = Console()
-    console.print("[bold cornflower_blue]CLIP[/bold cornflower_blue] : " + 
+    console.print("\n\n\t\t[bold light_sky_blue1]**************************************************[/bold light_sky_blue1]\n\n" +
+            "\t\t\t[bold cornflower_blue]CLIP[/bold cornflower_blue] : " + 
             "[bold cornflower_blue]C[/bold cornflower_blue][light_sky_blue1]ommand[/light_sky_blue1] " + 
             "[bold cornflower_blue]L[/bold cornflower_blue][light_sky_blue1]ine[/light_sky_blue1] " + 
             "[bold cornflower_blue]I[/bold cornflower_blue][light_sky_blue1]tem[/light_sky_blue1] " + 
-            "[bold cornflower_blue]P[/bold cornflower_blue][light_sky_blue1]riorities[/light_sky_blue1]")
+            "[bold cornflower_blue]P[/bold cornflower_blue][light_sky_blue1]riorities[/light_sky_blue1]\n\n" +
+            "\t\t[bold light_sky_blue1]**************************************************[bold light_sky_blue1]\n\n")
 
 
 if __name__=='__main__':
