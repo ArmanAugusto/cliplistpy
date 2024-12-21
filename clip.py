3f4eff8fd5de6b7556ec7691cbcc64593a60f989
Arman Augusto <arman.augusto@gmail.com>
2024-12-21 06:54:05 -0800

    started menu creation

--- clip.py
+++ clip.py
@@ -4,14 +4,24 @@ from rich.console import Console
 
 def title_header():
     console = Console()
-    console.print("\n\n\t\t[bold light_sky_blue1]**************************************************[/bold light_sky_blue1]\n\n" +
-            "\t\t\t[bold cornflower_blue]CLIP[/bold cornflower_blue] : " + 
-            "[bold cornflower_blue]C[/bold cornflower_blue][light_sky_blue1]ommand[/light_sky_blue1] " + 
-            "[bold cornflower_blue]L[/bold cornflower_blue][light_sky_blue1]ine[/light_sky_blue1] " + 
-            "[bold cornflower_blue]I[/bold cornflower_blue][light_sky_blue1]tem[/light_sky_blue1] " + 
-            "[bold cornflower_blue]P[/bold cornflower_blue][light_sky_blue1]riorities[/light_sky_blue1]\n\n" +
-            "\t\t[bold light_sky_blue1]**************************************************[bold light_sky_blue1]\n\n")
+    console.print(
+        "\n\n\t\t[bold light_sky_blue1]***********************" +
+        "***************************[/]\n\n" +
+        "\t\t\t[bold cornflower_blue]CLIP[/] : " + 
+        "[bold cornflower_blue]C[/][light_sky_blue1]ommand[/] " + 
+        "[bold cornflower_blue]L[/][light_sky_blue1]ine[/] " + 
+        "[bold cornflower_blue]I[/][light_sky_blue1]tem[/] " + 
+        "[bold cornflower_blue]P[/][light_sky_blue1]riorities[/]\n\n" +
+        "\t\t[bold light_sky_blue1]*********************************" +
+        "*****************[/]\n\n")
 
+
+def main_menu():
+    console = Console()
+    console.print(
+        "\n\n\t\t\t[bold cornflower_blue]1[/] " +
+        "- [light_sky_blue1]About CLIP[/]\n\n")
 
 if __name__=='__main__':
     title_header()
+    main_menu()
