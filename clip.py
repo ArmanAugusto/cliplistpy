dee74de2089055da4ca6fd43dc1dedb4833be4eb
Arman Augusto <arman.augusto@gmail.com>
2024-12-28 06:07:26 -0800

    added main_menu_actions method as a start

--- clip.py
+++ clip.py
@@ -6,7 +6,7 @@ from rich.markdown import Markdown
 def title_header():
     console = Console()
     console.print(
-        "\n\n\t\t[bold light_sky_blue1]***********************" +
+        "\n\n\n\n\t\t[bold light_sky_blue1]***********************" +
         "***************************[/]\n\n" +
         "\t\t\t[bold cornflower_blue]CLIP[/] : " + 
         "[bold cornflower_blue]C[/][light_sky_blue1]ommand[/] " + 
@@ -28,11 +28,23 @@ def about_clip():
 def main_menu():
     console = Console()
     console.print(
-        "\n\n\t\t\t[bold cornflower_blue]1[/] " +
+        "\n\t\t\t[bold cornflower_blue]1[/] " +
         "- [light_sky_blue1]About CLIP[/]\n\n")
+
+
+def main_menu_actions():
+    console = Console()
+    choice = console.input(
+        "\t\t\t[light_sky_blue1]menu option:  [/]")
+
+    console.print(f"\t\t\t[green]You chose[/] [bold cornflower_blue]{choice}[/]\n\n")
+
+
+
 
 if __name__=='__main__':
     title_header()
     main_menu()
+    main_menu_actions()
 
 
