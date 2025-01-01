a1b962878d7b3a2cc204dc92a497e8aafc25ac5e
Arman Augusto <arman.augusto@gmail.com>
2025-01-01 06:57:41 -0800

    added menu action to display text file

--- clip.py
+++ clip.py
@@ -1,45 +1,49 @@
 #!/bin/python3
 
 from rich.console import Console
-from rich.markdown import Markdown
+from rich.syntax import Syntax
 
 def title_header():
     console = Console()
     console.print(
-        "\n\n\n\n\t\t[bold light_sky_blue1]***********************" +
-        "***************************[/]\n\n" +
-        "\t\t\t[bold cornflower_blue]CLIP[/] : " + 
+        "\n\n\n\n[bold light_sky_blue1]***********************" +
+        "****************[/]\n\n" +
+        "  [bold cornflower_blue]CLIP[/] : " + 
         "[bold cornflower_blue]C[/][light_sky_blue1]ommand[/] " + 
         "[bold cornflower_blue]L[/][light_sky_blue1]ine[/] " + 
         "[bold cornflower_blue]I[/][light_sky_blue1]tem[/] " + 
         "[bold cornflower_blue]P[/][light_sky_blue1]riorities[/]\n\n" +
-        "\t\t[bold light_sky_blue1]*********************************" +
-        "*****************[/]\n\n")
+        "[bold light_sky_blue1]*********************************" +
+        "******[/]\n\n")
 
 
 def about_clip():
-    with open("README.md", "r") as f:
-        markdown_text = f.read()
+    console = Console()
+    with open("about_clip.txt", "r") as file:
+        text = file.read()
 
-    md = Markdown(markdown_text)
-    rich.print(md)
+    #syntax = Syntax(text, "text", theme="monokai", line_numbers=False)
+    console.print(text)
 
 
 def main_menu():
     console = Console()
     console.print(
-        "\n\t\t\t[bold cornflower_blue]1[/] " +
+        "\n\t[bold cornflower_blue]1[/] " +
         "- [light_sky_blue1]About CLIP[/]\n\n")
 
 
 def main_menu_actions():
     console = Console()
     choice = console.input(
-        "\t\t\t[light_sky_blue1]menu option:  [/]")
+        "    [light_sky_blue1]menu option:  [/]")
 
-    console.print(f"\t\t\t[green]You chose[/] [bold cornflower_blue]{choice}[/]\n\n")
-
-
+    # check validity of choice
+    if choice.isnumeric() and int(choice) in range(0, 2):
+        if choice == '1':
+            about_clip()
+    else:
+        print(f"{choice} is not a valid option from the menu")
 
 
 if __name__=='__main__':
